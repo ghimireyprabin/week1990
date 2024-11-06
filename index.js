f9769dd3bd5cc98ec60319e1b1df463935084704
Prabin <ghimirey.prabin@gmail.com>
2024-11-06 09:53:26 -0500

    fifth commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 const config = {
     user: 'azureuser',
     password: 'P@ssw0rdssss',
-    server: 'week1990.database.windows.net',
+    server: 'week1990server.database.windows.net',
     database: 'week1990db',
     options: {
         encrypt: true,
